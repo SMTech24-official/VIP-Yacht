e2f959f540449e286fdb30225024d8d3eabaf6c9
nazneen636 <nazneennahar636@gmail.com>
2025-03-08 14:13:51 +0600

    404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-fontColor"> Page not found.</h1>
-      <p className="text-xl text-gray-600 mt-4">
+    <div className="px-5 flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
+      <h1 className="text-3xl md:text-6xl font-bold text-fontColor">
+        {" "}
+        Page not found.
+      </h1>
+      <p className="text-xl text-gray-600 mt-4 text-center">
         Sorry, we couldn’t find the page you’re looking for.
       </p>
       <Link
